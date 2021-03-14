c4dcc0e8dce0ad0c1c7f8535ebfdb7be35b3ab39
José Marinho <68341332+clowdcap@users.noreply.github.com>
2021-03-14 13:23:26 -0300

    Update Front-End

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import {db} from './firebase.js';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 
 function App() {
 
+  const [user, setUser] = useState('Jose');
+
+
   useEffect(function(){
-    console.log(db);
+    
+    
+
   }, []);
 
 
@@ -15,7 +20,42 @@ function App() {
 
   return (
     <div className="App">
-      
+
+      <div className="header">
+
+        <div className="center">
+
+          <div className="header__logo">
+            <a href="#" className="logo-instagran">
+              <img alt="Instagram" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"/>
+            </a>
+          </div>
+
+          {
+            (user)?
+              <div className="header__loginForm">
+                <form>
+                  <input type="text" placeholder="Pesquisar"/>
+                  <input type="submit" name="acao" value="Deslogar"/>
+                </form>
+              </div>
+            :
+            <div className="header__loginForm">
+              <form>
+                <input type="text" placeholder="Login"/>
+                <input type="password" placeholder="Senha"/>
+                <input type="submit" name="acao" value="Logar"/>
+              </form>
+            </div>
+          }
+
+
+          
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
